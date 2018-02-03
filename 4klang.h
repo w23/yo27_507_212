d7523c1b8ff52869096e38d0e024ac14effe9619
Ivan Avdeev <me@w23.ru>
2018-02-03 13:26:32 +0700

    add new music

--- 4klang.h
+++ 4klang.h
@@ -1,31 +1,22 @@
 // some useful song defines for 4klang
 #define SAMPLE_RATE 44100
-#define BPM 60.000000
-#define MAX_INSTRUMENTS 5
-#define MAX_PATTERNS 17
-#define PATTERN_SIZE_SHIFT 5
+#define BPM 120.000000
+#define MAX_INSTRUMENTS 12
+#define MAX_PATTERNS 92
+#define PATTERN_SIZE_SHIFT 4
 #define PATTERN_SIZE (1 << PATTERN_SIZE_SHIFT)
 #define MAX_TICKS (MAX_PATTERNS*PATTERN_SIZE)
-#define SAMPLES_PER_TICK 11025
+#define SAMPLES_PER_TICK 5512
 #define MAX_SAMPLES (SAMPLES_PER_TICK*MAX_TICKS)
 #define POLYPHONY 2
 #define FLOAT_32BIT
 #define SAMPLE_TYPE float
 
-#define WINDOWS_OBJECT
+#define LINUX_OBJECT
 
 // declaration of the external synth render function, you'll always need that
-#ifdef __cplusplus
-extern "C"
-#endif
-void  __stdcall	_4klang_render(void*);
+extern void* __4klang_render(void*);
 // declaration of the external envelope buffer. access only if you're song was exported with that option
-#ifdef __cplusplus
-extern "C"
-#endif
-float _4klang_envelope_buffer;
+extern float __4klang_envelope_buffer;
 // declaration of the external note buffer. access only if you're song was exported with that option
-#ifdef __cplusplus
-extern "C"
-#endif
-int   _4klang_note_buffer;
+extern int   __4klang_note_buffer;
